f37457333f47e09b2c8eb0b311d63e56ba0846fe
Azmyin <azmyin12@gmail.com>
2024-04-16 21:22:14 -0500

    depth image figured out

--- README.md
+++ README.md
@@ -15,10 +15,21 @@ depth0 --> ```/robot_1/front_rgbd_camera/depth_registered/image_raw/compressedDe
 
 * ```rosrun my_package my_node.py _one_topic:="/topic1" _another_topic:="/topic2" __name:="my_node_name"```
 
+
+#### NOTES
+
+* Depth image from compressedDepth: https://answers.ros.org/question/249775/display-compresseddepth-image-python-cv2/
+
+* https://gist.github.com/awesomebytes/958a5ef9e63821a28dc05775840c34d9
+
 * Call signature
 
 1. KUKA robot only
 ```
 rosrun dataset_capture capture_node.py _r0_cam0:="/robot_0/front_rgbd_camera/rgb/image_rect_color/compressed" _r0_depth0:="/robot_0/front_rgbd_camera/depth/image_rect/compressedDepth" _r1_cam0:="/robot_1/front_rgbd_camera/rgb/image_rect_color/compressed" _r1_depth0:="/robot_1/front_rgbd_camera/depth/image_rect/compressedDepth"
 
+```
+
+```
+rosrun dataset_capture capture_node.py _r0_cam0:="/robot_0/front_rgbd_camera/rgb/image_rect_color/compressed" _r0_depth0:="/robot_0/front_rgbd_camera/depth_registered/image_raw/compressedDepth" _r1_cam0:="FOO" _r1_depth0:="BAR"
 ```
--- scripts/capture_node.py
+++ scripts/capture_node.py
@@ -11,11 +11,11 @@ import cv2 # OpenCV
 import tqdm # Prints a progress bar on console
 from pathlib import Path # To find the "home" directory location
 import shutil # High level folder operation tool
-
+import numpy as np
+import struct
 
 class DatasetCaptureNode:
     def __init__(self):
-        
         
         # Commandline parameters
         # Initialize ROS
@@ -43,13 +43,18 @@ class DatasetCaptureNode:
 
         # Setup subscribers
         self.robot0_cam0_sub_ = rospy.Subscriber(self.robot0_cam0, CompressedImage, self.robot0_cam0_callback)
-        # self.robot0_depth0_sub_ = rospy.Subscriber('robot0_depth0', Image, self.robot0_depth0_callback)
+        self.robot0_depth0_sub_ = rospy.Subscriber(self.robot0_depth0, CompressedImage, self.robot0_depth0_callback)
         
         # Setup directories
-        dataset_dir = str(Path.cwd()) + "/src/dataset_capture/dataset/"
-        print(dataset_dir)
-        # self.create_folder_once(dataset_dir)
-
+        dataset_dir = str(Path.home()) + "/DATASETS/data_capture/"
+        
+        if not os.path.exists(dataset_dir):
+            error_msg = "Error: Dataset directory does not exist: {}".format(dataset_dir)
+            raise FileNotFoundError(error_msg)
+            # You can customize the error type and message as needed
+        else:
+            print("Dataset directory exists:", dataset_dir)
+        
         # create the two folders as needed
         self.r0_cam0_dir = dataset_dir + "robot0/cam0/data/"
         self.r0_depth0_dir = dataset_dir + "robot0/depth0/data/"
@@ -68,26 +73,112 @@ class DatasetCaptureNode:
             rgb_img = self.br.compressed_imgmsg_to_cv2(data, "bgr8")
             # Convert the timestamp to a Python datetime object (if needed)
             rgb_timestamp = int(rospy.Time.to_sec(data.header.stamp) * 1000)
+            # print("Image timestamp:", rgb_timestamp)
             im_path = self.r0_cam0_dir + str(rgb_timestamp)+".png"
-            print(im_path)
+            # print(im_path)
         except CvBridgeError as e:
             print(e)
-        
-        # print("Image timestamp:", rgb_timestamp)
-        cv2.imwrite(im_path,rgb_img)
-        
-        # try:
-        #     cv2.imwrite(im_path,rgb_img)
-        # except:  # noqa: E722
-        #     print("Failed to write rgb image for robot0_cam0!!")
+
+        try:
+            #cv2.imwrite(im_path,rgb_img)
+            pass
+        except:  # noqa: E722
+            print("Failed to write rgb image for robot0_cam0!!")
 
         # Display the image
         cv2.imshow("Robot0 cam0", rgb_img)
         cv2.waitKey(1)  # Wait for a key press (1 millisecond)
         
 
-    def robot0_depth0_callback(self):
-        pass
+    def robot0_depth0_callback(self, msg):
+        """depth image callback for robot0"""
+        
+        print("depth0_callback")
+        
+        # 'msg' as type CompressedImage
+        depth_fmt, compr_type = msg.format.split(';')
+        # remove white space
+        depth_fmt = depth_fmt.strip()
+        compr_type = compr_type.strip()
+        if compr_type != "compressedDepth":
+            raise Exception("Compression type is not 'compressedDepth'."
+                            "You probably subscribed to the wrong topic.")
+
+        # remove header from raw data
+        depth_header_size = 12
+        raw_data = msg.data[depth_header_size:]
+
+        depth_img_raw = cv2.imdecode(np.fromstring(raw_data, np.uint8), cv2.IMREAD_UNCHANGED)
+        
+        if depth_img_raw is None:
+            # probably wrong header size
+            raise Exception("Could not decode compressed depth image."
+                            "You may need to change 'depth_header_size'!")
+
+        if depth_fmt == "16UC1":
+            print("Mono16 depth image")
+            # TODO write raw depth data
+        
+        elif depth_fmt == "32FC1":
+            print("Mono32 depth image")
+            raw_header = msg.data[:depth_header_size]
+            # header: int, float, float
+            [compfmt, depthQuantA, depthQuantB] = struct.unpack('iff', raw_header)
+            depth_img_scaled = depthQuantA / (depth_img_raw.astype(np.float32)-depthQuantB)
+            depth_img_scaled[depth_img_raw==0] = 0  # filter max values
+
+            # depth_img_scaled provides distance in meters as f32
+            # for storing it as png, we need to convert it to 16UC1 again (depth in mm)
+            depth_img_mm = (depth_img_scaled*1000).astype(np.uint16)
+            
+            # cv2.imwrite()
+
+            depth_img_mm_8bit = cv2.normalize(depth_img_mm, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            cv2.imshow("Robot0 depth0", depth_img_mm_8bit)
+            cv2.waitKey(1)  # Wait for a key press (1 millisecond)
+
+            try:
+                pass
+            except:
+                print("Failed to write rgb image for robot1_cam0!!")
+            
+            #cv2.imwrite(os.path.join(path_depth, "depth_" + str(msg.header.stamp) + ".png"), depth_img_mm)
+        else:
+            raise Exception("Failed to decode depth data")
+
+    #! EXPERIMENTAL, trying the top function first
+    def convert_compressedDepth_to_cv2(self, compressed_depth):
+        """
+        Convert a compressedDepth topic image into a cv2 image.
+        compressed_depth must be from a topic /bla/compressedDepth
+        as it's encoded in PNG
+        Code from: https://answers.ros.org/question/249775/display-compresseddepth-image-python-cv2/
+        """
+        depth_fmt, compr_type = compressed_depth.format.split(';')
+        # remove white space
+        depth_fmt = depth_fmt.strip()
+        compr_type = compr_type.strip()
+        if compr_type != "compressedDepth":
+            raise Exception("Compression type is not 'compressedDepth'."
+                            "You probably subscribed to the wrong topic.")
+
+        # remove header from raw data, if necessary
+        if 'PNG' in compressed_depth.data[:12]:
+            # If we compressed it with opencv, there is nothing to strip
+            depth_header_size = 0
+        else:
+            # If it comes from a robot/sensor, it has 12 useless bytes apparently
+            depth_header_size = 12
+        raw_data = compressed_depth.data[depth_header_size:]
+
+        depth_img_raw = cv2.imdecode(np.frombuffer(raw_data, np.uint8),
+                                     # the cv2.CV_LOAD_IMAGE_UNCHANGED has been removed
+                                     -1)  # cv2.CV_LOAD_IMAGE_UNCHANGED)
+        if depth_img_raw is None:
+            # probably wrong header size
+            raise Exception("Could not decode compressed depth image."
+                            "You may need to change 'depth_header_size'!")
+        return depth_img_raw
 
     def create_folder_once(self, folder_name_with_path):
         """Creates folder if it does not exsist."""
@@ -117,7 +208,6 @@ class DatasetCaptureNode:
 # Main entry point of the script
 if __name__ == '__main__':
     node = DatasetCaptureNode()
-    
     try:
         rospy.spin()
     except rospy.ROSInterruptException:
